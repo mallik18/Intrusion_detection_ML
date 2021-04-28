772422c3e6b06d365c28178e195828de4cabf822
mallik18 <mallikarjunkolar18@gmail.com>
2021-04-28 21:46:32 +0530

    made changes to icmp_attack file and train file

--- src/train_test_python_files/train_model.py
+++ src/train_test_python_files/train_model.py
@@ -4,34 +4,45 @@ import sys
 import pickle
 from sklearn.neighbors import KNeighborsClassifier
 
+
 print('TRAINING OUR MODEL "python train.py icmp 0" this will actually train the data set on icmp protocol with the KNN model.')
 print('If you wish to use the second model type "python train.py icmp 1" it uses DecisionTreeClassifier.')
 
 df = pd.read_csv("/home/noroot/Documents/final_year_project/Intrusion_detection_ML/dataset/revised_kddcup_dataset.csv",index_col=0)
 
 def train_icmp(df, classifier=0):
-    
+
     icmp_df = df[df.loc[:,"protocol_type"] == "icmp"]
-    icmp_feat = ["duration","src_bytes","wrong_fragment","count","urgent","num_compromised","srv_count"]
+    icmp_feat = ["duration","service","src_bytes","wrong_fragment","count","urgent","num_compromised","srv_count"]
     icmp_target = "result"
     X = icmp_df.loc[:,icmp_feat]
     y = icmp_df.loc[:,icmp_target]
+    
+    X['duration'] = X['duration'].astype('float')
+    X['src_bytes'] = X['src_bytes'].astype('float')
+    X['wrong_fragment'] = X['wrong_fragment'].astype('float')
+    X['count'] = X['count'].astype('float')
+    X['urgent'] = X['urgent'].astype('float')
+    X['num_compromised'] = X['num_compromised'].astype('float')
+    X['srv_count'] = X['srv_count'].astype('float')
+    
     classes = np.unique(y)
     for i in range(len(classes)):
         if i == 2:
-            icmp_df = icmp_df.replace(classes[i], 0)
+            X = X.replace(classes[i], 0)
         else:
-            icmp_df = icmp_df.replace(classes[i], 1)
+            X = X.replace(classes[i], 1)
 
     #turning the service attribute to categorical values
-    #icmp_df=icmp_df.replace("eco_i",-0.1)
-    #icmp_df=icmp_df.replace("ecr_i",0.0)
-    #icmp_df=icmp_df.replace("tim_i",0.1)
-    #icmp_df=icmp_df.replace("urp_i",0.2)
+    X = X.replace("eco_i",-0.1)
+    X = X.replace("ecr_i",0.0)
+    X = X.replace("tim_i",0.1)
+    X = X.replace("urp_i",0.2)
     
+
     y = icmp_df.loc[:,icmp_target] #updating the y variables
     print("Data preprocessing done.")
-    
+
     #choose KNN if classifier == 0 else choose ID3
     if str(classifier) == "0":
         k = 3
